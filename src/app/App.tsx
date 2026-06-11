f8da65c4b2d9c1dac2c7b14488b25b632636e875
Alexander Stasyuk <predator12007@mail.ru>
2026-06-11 14:02:27 +0300

    global classes for color switching

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,15 @@ import {selectThemeMode} from './app-slice';
 import {getTheme} from '../common/theme/theme';
 import {CssBaseline} from '@mui/material';
 import {Router} from './routing';
+import {useEffect} from 'react';
 
 export const App = () => {
     const themeMode = useAppSelector(selectThemeMode)
     const theme = getTheme(themeMode)
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', themeMode)
+    }, [themeMode])
 
     return (
         <ThemeProvider theme={theme}>
